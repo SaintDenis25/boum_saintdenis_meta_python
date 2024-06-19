d41da09d43c7445b29a9b23b0ac0c4d4579e79d7
SaintDenis25 <94716271+SaintDenis25@users.noreply.github.com>
2024-06-19 16:16:27 +0200

    Delete translator.py

--- translator.py
+++
@@ -1,7 +0,0 @@
-
-from googletrans import Translator
-
-def translate_text(text, dest_lang):
-    translator = Translator()
-    translation = translator.translate(text, dest=dest_lang)
-    return translation.text
